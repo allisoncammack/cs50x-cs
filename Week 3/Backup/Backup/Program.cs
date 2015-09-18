b21baa71650fd1f5a0dad042925f6f6e9c1c74a3
Allison Cammack <allisoncammack@gmail.com>
2015-09-18 10:37:27 -0400

    Week 3
    Week 3

---
+++ Week 3/Backup/Backup/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BackupFolder();
+            Console.ReadLine();
+        }
+
+        private static void BackupFolder()
+        {
+            // create zip file using current date
+            using (FileStream zipToOpen = File.Open(DateTime.Now.ToString() + Directory.GetCurrentDirectory() 
+                + ".zip", FileMode.Create))
+            {
+                // create archive 
+                using (ZipArchive archive = new ZipArchive(zipToOpen, ZipArchiveMode.Create))
+                {
+                    // get list of files to be zipped, using current directory
+                    var fileList = Directory.GetFiles((Directory.GetCurrentDirectory()));
+
+                    // loop through each file in current directory and zip
+                    foreach (var file in fileList)
+                    {
+                        archive.CreateEntryFromFile(file, file);
+                    }
+
+                // back up new zip file to folder
+                    // FINISH - REPLACE NAME WITH NAME OF BACKUP DIRECTORY? NOW USING MSDN STRING NAME
+                archive.ExtractToDirectory(Directory.GetCurrentDirectory());
+                }
+            }
+        }
+    }
+}
---
+++ Week 3/EnvironmentProperties/EnvironmentProperties/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnvironmentProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MachineName: {0}", Environment.MachineName);
+            Console.WriteLine("ProcessorCount: {0}", Environment.ProcessorCount);
+            Console.WriteLine("Is64BitOperatingSystem: {0}", Environment.Is64BitOperatingSystem);
+            Console.WriteLine("OSVersion: {0}", Environment.OSVersion);
+            Console.ReadLine();
+        }
+    }
+}
